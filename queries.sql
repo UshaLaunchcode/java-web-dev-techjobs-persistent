a251205b72d4321b27b11b2a749278de54b19f0a
UshaLaunchcode <usha_ece@yahoo.com>
2021-03-15 17:18:12 -0500

    Finished Part1:Connect a Database to a Spring App of Assignment #4: Tech Jobs (Persistent Edition)

--- queries.sql
+++ queries.sql
@@ -1,5 +1,11 @@
 ## Part 1: Test it with SQL
 
+Job: Column (Datatype):
+id       (int)
+employer (String)
+name     (String)
+skills   (String)
+
 ## Part 2: Test it with SQL
 
 ## Part 3: Test it with SQL
