ced60c65ccf625962b9ee897239f644b12678921
UshaLaunchcode <usha_ece@yahoo.com>
2021-03-15 20:47:00 -0500

    Finished until  Part 2: Persisting Employers and Skills Models Step:1 of Assignment #4: Tech Jobs (Persistent Edition)

--- queries.sql
+++ queries.sql
@@ -1,13 +1,13 @@
-## Part 1: Test it with SQL
+--## Part 1: Test it with SQL
 
-Job: Column (Datatype):
-id       (int)
-employer (String)
-name     (String)
-skills   (String)
+--Job: Column (Datatype):
+--id       (int)
+--employer (String)
+--name     (String)
+--skills   (String)
 
-## Part 2: Test it with SQL
+--## Part 2: Test it with SQL
 
-## Part 3: Test it with SQL
+--## Part 3: Test it with SQL
 
-## Part 4: Test it with SQL
+--## Part 4: Test it with SQL
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,19 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 public class Employer extends AbstractEntity {
 
+    @NotBlank(message="Location is required")
+    @Size(min=2, message="Location must be at least 2 characters long")
+    private String location;
 
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
